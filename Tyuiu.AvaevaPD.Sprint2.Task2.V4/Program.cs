62742fe48f79e2c01ae7aeaae0b4b116fce13ac6
polika1122@mail.ru <polika1122@mail.ru>
2023-10-18 15:31:27 +0500

    Task2 complete

---
+++ Tyuiu.AvaevaPD.Sprint2.Task2.V4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AvaevaPD.Sprint2.Task2.V4.Lib;
+
+namespace Tyuiu.AvaevaPD.Sprint2.Task2.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: " );
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y:  ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+           
+            if (res)
+            {
+                Console.WriteLine("Точка находиться в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находиться в заштрихованной области");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
